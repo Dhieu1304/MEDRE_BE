252aed9a5e88914df89f5838c159bbe57cc2760f
Lindonut <lindonut10@gmail.com>
2023-04-19 16:21:55 +0700

    new feature: sms otp (haven't finished yet)

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const ApiError = require('../utils/ApiError');
 
 const envVarsSchema = Joi.object()
   .keys({
@@ -22,11 +23,14 @@ const envVarsSchema = Joi.object()
     NODEMAILER_EMAIL: Joi.string().required().description('Email'),
     NODEMAILER_PASSWORD: Joi.string().required().description('Password'),
     BE_URL: Joi.string().required().description('Url of backend'),
-    FE_URL: Joi.string().required().description('Url of frontend'),
+    FE_USER_URL: Joi.string().required().description('Url of user frontend'),
+    FE_ADMIN_URL: Joi.string().required().description('Url of admin frontend'),
     FB_PRIVATE_KEY_ID: Joi.string().required().description('Private key id Firebase Admin'),
     FB_PRIVATE_KEY: Joi.string().required().description('Private key Firebase Admin'),
     VNP_TMN_CODE: Joi.string().required().description('VNPay tmn code'),
     VNP_HASH_SECRET: Joi.string().required().description('VNPay hash secret'),
+    SMS_API_KEY: Joi.string().required().description('SMS API key'),
+    SMS_SECRET_KEY: Joi.string().required().description('SMS secret key'),
   })
   .unknown();
 
@@ -75,7 +79,8 @@ module.exports = {
   },
   base_url: {
     be_url: envVars.BE_URL,
-    fe_url: envVars.FE_URL,
+    fe_user_url: envVars.FE_USER_URL,
+    fe_admin_url: envVars.FE_ADMIN_URL,
   },
   firebaseAdmin: {
     privateKeyId: envVars.FB_PRIVATE_KEY_ID,
@@ -87,5 +92,9 @@ module.exports = {
     hashSecret: envVars.VNP_HASH_SECRET,
     url: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
     returnUrl: `${envVars.BE_URL}/payment/vnpay-return`,
+  },
+  sms: {
+    apiKey: envVars.SMS_API_KEY,
+    secretKey: envVars.SMS_SECRET_KEY,
   },
 };
---
+++ src/otp/sms.js
@@ -0,0 +1,60 @@
+const config = require('../config');
+const https = require('https');
+//const apiKey = config.sms.apiKey;
+//const apiSecret = config.sms.secretKey;
+const data = (Phone) => {
+    const postData = JSON.stringify({
+        "ApiKey": `${config.sms.apiKey}`,
+        "Content": "Noi dung sms",
+        "Phone": `${Phone}`,
+        "SecretKey": `${config.sms.secretKey}`,
+        "SmsType": "8",
+        "SandBox":0,
+    });
+    return postData;
+}
+
+//const dt = data('0933021021');
+//console.log(dt);
+
+
+const sendSMS = (Phone) => {
+    const url = 'rest.esms.vn';
+    const options = {
+        host: url,
+        port: 443,
+        path: '/MainService.svc/json/SendMultipleMessage_V4_post_json/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        
+    };
+
+    const req = https.request(options, function(res) {
+        res.setEncoding('utf8');
+        var body = '';
+        res.on('data', function(d) {
+            body += d;
+        });
+        res.on('end', function() {
+            var json = JSON.parse(body);
+            console.log("Result: " , json);
+            // if (json.status == 'success') {
+            //     console.log("Send sms success.")
+            // }
+            // else {
+            //     console.log("Send sms failed: " + body);
+            // }
+        });
+    });
+
+    req.on('error', function(e) {
+        console.log("Send sms failed: " + e);
+    });
+    const dt = data(Phone)
+    req.write(dt);
+    req.end();
+}
+
+//sendSMS('0933021021');
