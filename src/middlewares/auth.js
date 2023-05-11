0efe5781367f555c4ff11d86bbcd9a01a545a9fe
Dhieu1304 <d.hieu.13.04@gmail.com>
2023-05-11 22:18:11 +0700

    maintain auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,11 +3,25 @@ const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const i18next = require('i18next');
 const { isBlockedAccount } = require('../nodeCache/account');
+const { getGlobalSetting } = require('../nodeCache/globalSetting');
+const { STAFF_ROLES } = require('../staff/staff.constant');
 
 const verifyCallback = (req, resolve, reject) => {
   return async (err, user, info) => {
     if (err || info || !user) {
       return reject(new ApiError(httpStatus.UNAUTHORIZED, i18next.t('error.unauthorized')));
+    }
+
+    if (user.role !== STAFF_ROLES.ADMIN) {
+      const globalSetting = await getGlobalSetting();
+      for (let i = 0; i < globalSetting.length; i++) {
+        if (globalSetting[i].name === 'maintain') {
+          if (globalSetting[i].value === '1') {
+            return reject(new ApiError(httpStatus.BAD_REQUEST, i18next.t('error.maintain')));
+          }
+          break;
+        }
+      }
     }
 
     if (isBlockedAccount(user.id)) {
