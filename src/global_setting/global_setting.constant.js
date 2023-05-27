1f10b475ea8a21e099602339854d052d92b5d6ec
Dhieu1304 <d.hieu.13.04@gmail.com>
2023-05-27 23:37:48 +0700

    add global setting booking

--- src/global_setting/global_setting.constant.js
+++ src/global_setting/global_setting.constant.js
@@ -4,7 +4,6 @@ const GLOBAL_SETTING = Object.freeze({
   BOOK_ADVANCE_DAY: 'bookAdvanceDay',
   BOOK_AFTER_DAY: 'bookAfterDay',
   CREATE_SCHEDULE_ADVANCE_DAY: 'createScheduleAdvanceDay',
-  SCHEDULE_DAY_CAN_EDIT: 'scheduleDayCanEdit',
 });
 
 module.exports = {
