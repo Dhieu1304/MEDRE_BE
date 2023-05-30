b3dfde38769dec3dc141736007f335001ca14db7
Lindonut <lindonut10@gmail.com>
2023-05-30 13:42:21 +0700

    update feature: checkup package

--- src/checkup_package/checkup_package.route.js
+++ src/checkup_package/checkup_package.route.js
@@ -1,5 +1,38 @@
 const express = require('express');
+const checkupPackageController = require('./checkup_package.controller');
+const validate = require('../middlewares/validate');
+const checkupPackageValidation = require('./checkup_package.validation');
+const { staffPermission } = require('../middlewares/staffPermission');
+const { ALL_STAFF_ROLES, STAFF_ROLES } = require('../staff/staff.constant');
 
 const router = express.Router();
+
+router.get(
+    '/list-checkup-package', 
+    staffPermission(ALL_STAFF_ROLES),
+    validate(checkupPackageValidation.listCheckupPackage), 
+    checkupPackageController.getAllCheckupPackage
+);
+router.get(
+    '/expertise/:id', 
+    checkupPackageController.getAllCheckupPackageByExpertise
+);
+router.post(
+    '/update-checkup-package/:id', 
+    staffPermission(ALL_STAFF_ROLES),
+    validate(checkupPackageValidation.updateCheckupPackage), 
+    checkupPackageController.updateCheckupPackage
+);
+router.post(
+    '/create-checkup-package', 
+    staffPermission(ALL_STAFF_ROLES),
+    validate(checkupPackageValidation.createPackage), 
+    checkupPackageController.createCheckupPackage
+);
+router.post(
+    '/delete-checkup-package/:id',  
+    staffPermission(ALL_STAFF_ROLES),
+    checkupPackageController.deleteCheckupPackage
+);
 
 module.exports = router;
--- src/checkup_package/checkup_package.service.js
+++ src/checkup_package/checkup_package.service.js
@@ -3,6 +3,9 @@ const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const i18next = require('i18next');
 const { v4: uuidv4 } = require('uuid');
+const bookingService = require('../booking/booking.service');
+const moment = require('moment');
+const { Op } = require('sequelize');
 
 const findOneByFilter = async (filter) => {
   return await models.checkup_package.findOne({ where: filter });
@@ -12,13 +15,24 @@ const findAllByFilter = async (filter) => {
   return await models.checkup_package.findAll({ where: filter });
 };
 
-const updatePrice = async (id_checkup_package, price) => {
-  const checkup_package = await findOneByFilter({ id: id_checkup_package });
+const findAndCountAllByCondition = async (condition) => {
+  return await models.checkup_package.findAndCountAll(condition);
+};
+
+const updateCheckupPackage = async (id_checkup_package, data) => {
+  let checkup_package = await findOneByFilter({ id: id_checkup_package });
   if (!checkup_package) {
     throw new ApiError(httpStatus.BAD_REQUEST, i18next.t('checkupPackage.notFound'));
   }
-  await checkup_package.update({ price: price });
-  return checkup_package;
+  let filter = Object.assign({}, { date: { [Op.gt]: moment().subtract(1, 'd')} });
+  filter.id_checkup_package = id_checkup_package;
+  const booking = await bookingService.findAllByFilter(filter);
+  if (booking.length > 0)
+  {
+    throw new ApiError(httpStatus.BAD_REQUEST, i18next.t('checkupPackage.cannotUpdate'));
+  }
+  checkup_package = Object.assign(checkup_package, data);
+  return await checkup_package.save();
 };
 
 const createCheckupPackage = async (data) => {
@@ -26,7 +40,7 @@ const createCheckupPackage = async (data) => {
   data.id = uuidv4();
 
   // create new user
-  return models.package.create(data);
+  return models.checkup_package.create(data);
 };
 
 const deleteCheckupPackage = async (data) => {
@@ -34,7 +48,14 @@ const deleteCheckupPackage = async (data) => {
   if (!checkup_package) {
     throw new ApiError(httpStatus.BAD_REQUEST, i18next.t('checkupPackage.notFound'));
   }
-
+  
+  let filter = Object.assign({}, { date: { [Op.gt]: moment().subtract(1, 'd')} });
+  filter.id_checkup_package = data;
+  const booking = await bookingService.findAllByFilter(filter);
+  if (booking.length > 0)
+  {
+    throw new ApiError(httpStatus.BAD_REQUEST, i18next.t('checkupPackage.cannotDelete'));
+  }
   // delete package
   return await models.checkup_package.destroy({ where: { id: checkup_package.id } });
 };
@@ -42,7 +63,8 @@ const deleteCheckupPackage = async (data) => {
 module.exports = {
   findOneByFilter,
   findAllByFilter,
-  updatePrice,
+  findAndCountAllByCondition,
+  updateCheckupPackage,
   createCheckupPackage,
   deleteCheckupPackage,
 };
