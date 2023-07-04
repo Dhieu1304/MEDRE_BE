3791b28398fd6161b42cd20b28dd3d273223b240
Lindonut <lindonut10@gmail.com>
2023-07-04 20:31:44 +0700

    update custom validate email address

--- src/utils/validateCustom.js
+++ src/utils/validateCustom.js
@@ -2,6 +2,7 @@ const i18next = require('i18next');
 
 const phoneNumberRegex = /((^(\+84|84|0|0084){1})(3|5|7|8|9))+([0-9]{8})$/;
 const timeScheduleRegex = /^([0-9]{2}):(00|30):(00)$/;
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 const password = (value, helpers) => {
   if (value.length < 8) {
@@ -17,4 +18,5 @@ module.exports = {
   phoneNumberRegex,
   timeScheduleRegex,
   password,
+  emailRegex,
 };
